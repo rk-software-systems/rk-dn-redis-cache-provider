7f2e731030986925dcc8152b4ff62ed4f6668d22
Roman St <fcromich@gmail.com>
2023-02-07 15:06:06 +0100

    feat: minor fix

--- RKSoftware.Packages.Caching/Constants/LoggingConstants.cs
+++ RKSoftware.Packages.Caching/Constants/LoggingConstants.cs
@@ -3,22 +3,22 @@ namespace RKSoftware.Packages.Caching
 {
     internal static class LoggingConstants
     {
-        public const int RedisObjectNotFoundWarning           = 600000;
-        public const int RedisGetObjectConnectionError        = 600001;
-        public const int RedisSetObjectError                  = 600002;
-        public const int RedisConnectionError                 = 600003;
-        public const int RedisGetObjectError                  = 600004;
-        public const int RedisGetObjectInformation            = 600005;
-        public const int RedisSetObjectConnectionError        = 600006;
-        public const int RedisSetObjectInformation            = 600007;
-        public const int RedisResetConnectionError            = 600008;
-        public const int RedisRemoveObjectError               = 600009;
-        public const int RedisBulkResetConnectionError        = 600010;
-        public const int RedisBulkResetError                  = 600011;
-        public const int RedisBulkPartialResetConnectionError = 600012;
-        public const int RedisBulkPartialReset                = 600013;
-        public const int RedisConnectionOpeneningInformation  = 600014;
-        public const int RedisConnectionOpenError             = 600015;
+        internal const int RedisObjectNotFoundWarning           = 600000;
+        internal const int RedisGetObjectConnectionError        = 600001;
+        internal const int RedisSetObjectError                  = 600002;
+        internal const int RedisConnectionError                 = 600003;
+        internal const int RedisGetObjectError                  = 600004;
+        internal const int RedisGetObjectInformation            = 600005;
+        internal const int RedisSetObjectConnectionError        = 600006;
+        internal const int RedisSetObjectInformation            = 600007;
+        internal const int RedisResetConnectionError            = 600008;
+        internal const int RedisRemoveObjectError               = 600009;
+        internal const int RedisBulkResetConnectionError        = 600010;
+        internal const int RedisBulkResetError                  = 600011;
+        internal const int RedisBulkPartialResetConnectionError = 600012;
+        internal const int RedisBulkPartialReset                = 600013;
+        internal const int RedisConnectionOpeneningInformation  = 600014;
+        internal const int RedisConnectionOpenError             = 600015;
 
     }
 }
